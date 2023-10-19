7d04b12805a523a812e4610f55432507defe8780
Dima Tytenko <amgpetronass@gmail.com>
2023-10-19 14:18:01 +0300

    remove first initialization of simpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,11 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-import {
-  createGalleryMarkup,
-  getLightboxMurkup,
-  onGalleryContainerClick,
-} from "./functions.js";
+import { createGalleryMarkup, getLightboxMurkup } from "./functions.js";
 
 const galleryContainer = document.querySelector(".gallery");
 
@@ -13,9 +9,7 @@ const galleryMarkup = createGalleryMarkup(galleryItems, getLightboxMurkup);
 
 galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 
-const instance = new SimpleLightbox(".gallery a", {
+new SimpleLightbox(".gallery a", {
   captionsData: "alt",
   captionDelay: 250,
 });
-
-instance.open();
